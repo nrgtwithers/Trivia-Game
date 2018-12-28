266a05b014b84906642e3bcdae38e812501acee2
Nina Withers <ninarosewithers@gmail.com>
2018-12-28 15:56:39 -0500

    Adding changes to css using jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,42 +2,42 @@
 
 var randomQuestions = [{
     question: "Which sea creature has three hearts?",
-    choices: ["Shark", "Octopus", "Humpback Whale", "Shrimp"],
+    choices: ["  Shark  ", "  Octopus  ", "  Humpback Whale  ", "  Shrimp  "],
     answer: 1,
 },
 {
     question: "How many bones does an adult human have?",
-    choices: ["300", "99", "138", "206"],
+    choices: ["  300  ", "  99  ", "  138  ", "  206  "],
     answer: 3,
 },
 {
     question: "What is the Italian word for pie?",
-    choices: ["Pizza", "Alici", "Bere", "Capra"],
+    choices: ["  Pizza  ", "  Alici  ", "  Bere  ", "  Capra  "],
     answer: 0,
 },
 {
     question: "What was Marilyn Monroe's natural hair colour?",
-    choices: ["Honey Blonde", "Brunette", "Ginger", "Dark Blonde"],
+    choices: ["  Honey Blonde  ", "  Brunette  ", "  Ginger  ", "  Dark Blonde  "],
     answer: 2,
 },
 {
     question: "Name the port of Rome.",
-    choices: ["Ostia", "Ravena", "Gioia Tauro", "Messina"],
+    choices: ["  Ostia  ", "  Ravena  ", "  Gioia Tauro  ", "  Messina  "],
     answer: 0,
 },
 {
     question: "Who sang about being an eggman and a walrus?",
-    choices: ["Elvis", "Cher", "The Beatles", "Michael Jackson"],
+    choices: ["  Elvis  ", "  Cher  ", "  The Beatles  ", "  Michael Jackson  "],
     answer: 2,
 },
 {
     question: "When did the French Revolution end?",
-    choices: ["1898", "1900", "1799", "1805"],
+    choices: ["  1898  ", "  1900  ", "  1799  ", "  1805  "],
     answer: 2,
 },
 {
     question: " Which Russian town suffered an infamous nuclear disaster in 1986?",
-    choices: ["Chernobyl", "Moscow", "Kazan", "Samara"],
+    choices: ["  Chernobyl  ", "  Moscow  ", "  Kazan  ", "  Samara  "],
     answer: 0,
 },
 ];
@@ -51,10 +51,10 @@ var correctAnswers=0;
 var incorrectAnswers=0;
 
 for (var i = 0; i < randomQuestions.length; i++) {
-    $("#questionArea").append(`<p>${randomQuestions[i].question}</p>`);
+    $("#questionArea").append(`<br><p>${randomQuestions[i].question}</p>`);
     for (var j = 0; j < randomQuestions[i].choices.length; j++) {
 
-        $("#questionArea").append(`<input type="radio" name="question${i}" questionNumber=${i} value="${randomQuestions[i].choices[j]}" correctAnswer=${randomQuestions[i].answer} class='answers'>${randomQuestions[i].choices[j]}`)
+        $("#questionArea").append(`<input type="radio" name="question${i}" questionNumber=${i} value="${randomQuestions[i].choices[j]}" correctAnswer=${randomQuestions[i].answer} class='answers'>${randomQuestions[i].choices[j]}<br>`)
     }
 }
 
@@ -72,9 +72,8 @@ function setTimer() {
             if (playAgain) {
                 counter = 60;
                 score =0;
-                $("#correctAnswers").text(`Correct Answers: ${correctAnswers}`)
-                $("#incorrectAnswers").text(`Incorrect Answers: ${incorrectAnswers}`)
-                $("#scores").text(`Your score is ${score}`)
+
+                
                 $("#questionArea").show()
             } else {
                 clearInterval(timer)
